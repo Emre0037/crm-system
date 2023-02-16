6f86a2f5dff57c614bf727410eefc6187ba2b9f6
emre037 <0319003@student.rocvantwente.nl>
2023-02-16 23:57:35 +0100

    crm-system

--- var/cache/dev/easyadmin/routes-dashboard.php
+++
@@ -1,5 +0,0 @@
-<?php return array (
-  'admin' => 'App\\Controller\\Admin\\DashboardController::index',
-  'app_home' => 'App\\Controller\\HomeController::index',
-  'messages.app_message_index' => 'App\\Controller\\MessageController::index',
-);
